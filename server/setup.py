cf24191720ad4d5b04af3cf036ded612bd8563b0
popov-p <pashafizmat1@gmail.com>
2024-06-27 15:47:20 +0300

    final repo state

---
+++ server/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='http_server',
+    version='1.0.0',
+    author_email='pashafizmat1@gmail.com',
+    description='DB GUI client and HTTP server',
+    zip_safe=False,
+    packages=['http_server'],
+)
